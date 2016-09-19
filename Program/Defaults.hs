3c0e3059455938b819478ecde7e529f7f4eb7070
Martin Hecker <martin.hecker@kit.edu>
2016-09-19 19:18:10 +0200

    tweak defaults

--- Program/Defaults.hs
+++ Program/Defaults.hs
@@ -10,8 +10,8 @@ import Unicode
 import Data.Graph.Inductive
 
 
-defaultInput  = Map.fromList [ (stdIn, cycle [ 2, 4, 6, 8]), (lowIn1, cycle [1,2,3,4]), (lowIn2, cycle [4,3,2,1]) ]
-defaultInput' = Map.fromList [ (stdIn, cycle [-8,-6,-4,-2]), (lowIn1, cycle [1,2,3,4]), (lowIn2, cycle [4,3,2,1]) ]
+defaultInput  = Map.fromList [ (stdIn, cycle [ 2, 1]), (lowIn1, cycle [1,2,3,4]), (lowIn2, cycle [4,3,2,1]) ]
+defaultInput' = Map.fromList [ (stdIn, cycle [-1, 0]), (lowIn1, cycle [1,2,3,4]), (lowIn2, cycle [4,3,2,1]) ]
 
 
 defaultChannelObservability channel

10bcfc7041f29b4cd21884f4d12224115ef9f766
Martin Hecker <martin.hecker@kit.edu>
2019-04-24 18:22:12 +0200

    use not-necessarily-connected graphs by default

--- Program/Properties/SASProperties.hs
+++ Program/Properties/SASProperties.hs
@@ -1,19 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE CPP #-}
 
--- #define USEUNCONNECTED
-#ifdef USEUNCONNECTED
-#define ARBITRARY(g) (g) :: (Gr () ())
-#else
+-- #define USECONNECTED
+#ifdef USECONNECTED
 #define ARBITRARY(g) (CG _ g) :: (Connected Gr () ())
+#else
+#define ARBITRARY(g) (g) :: (Gr () ())
 #endif
 
 #define UNCONNECTED(g) (g) :: (Gr () ())
-#define CONNECTED(g) (CG _ g) :: (Connected Gr () ())
-#define REDUCIBLE(g) (RedG g) :: (Reducible Gr () ())
-#define INTER(g) (InterGraph g) :: (InterGraph () String)
-#define INTERCFG(g) (InterCFG _ g) :: (InterCFG (Node) (Node, Node))
-#define SIMPLECFG(g) (SimpleCFG g) :: (SimpleCFG Gr)
 
 module Program.Properties.SASProperties where
 

31c3aa3fa58ed84a92ae16566d6fac6cc8626032
Martin Hecker <martin.hecker@kit.edu>
2019-05-24 19:32:38 +0200

    lets lock instead of delaying

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,9 @@
 shopt -s expand_aliases
 set -x
 timm() {
+  dotlockfile -l -r 100 ghc-lock
   /usr/bin/time -f '\t%E real,\t%U user,\t%S sys,\t%K amem,\t%M mmem' "$@"
+  dotlockfile -u        ghc-lock
 }
 export -f timm
 
@@ -10,6 +12,7 @@ set +e
 make fgl-control-order-dependence.cabal
 make clean
 cabal install --only-dependencies
-parallel --delay 120 --jobs 8 < jenkins_parallel.sh
+rm -f ghc-lock
+parallel --jobs 8 < jenkins_parallel.sh
 timm             make dist/build/sensitiveDom.valid.xml.bin/sensitiveDom.valid.xml     RTS="+RTS -M22288m     -RTS" PATTERN="-p '**/Properties/**'"
 timm             make dist/build/all.invalid.xml.bin/all.invalid.xml                   RTS="+RTS -M22288m -N8 -RTS"

95ae9c9c6cde8e01b6b29fdf8a69d895cfea1eb2
Martin Hecker <martin.hecker@kit.edu>
2019-06-26 16:49:32 +0200

    concrete edge semantics done

--- src/main/CacheExecution.hs
+++ src/main/CacheExecution.hs
@@ -10,6 +10,7 @@ import qualified Data.Map as Map
 
 import Control.Exception.Base (assert)
 import Control.Monad.State
+import Control.Monad.List
 
 
 import Unicode
@@ -42,6 +43,13 @@ cacheAwareReadLRU var σ@((globalσ,tlσ,i), cache) = case var of
             Nothing  -> let val = someσ ! var in (val, OMap.fromList $ (var, val) : (take (cacheSize - 1) $ OMap.assocs                   cache) )
             Just val ->                          (val, OMap.fromList $ (var, val) : (                       OMap.assocs $ OMap.delete var cache) )
 
+cacheAwareReadLRUState :: Monad m => Var -> StateT FullState m Val
+cacheAwareReadLRUState var = do
+    σ@((globalσ,tlσ,i), cache) <- get
+    let (val, cache') = cacheAwareReadLRU var σ
+    put ((globalσ,tlσ,i), cache')
+    return val
+
 cacheAwareWriteLRU :: Var -> Val -> FullState -> FullState
 cacheAwareWriteLRU var val σ@((globalσ,tlσ,i), cache) = case var of
     Global      _ -> let (globalσ', cache') = write globalσ in ((globalσ',tlσ ,i), cache')
@@ -52,6 +60,14 @@ cacheAwareWriteLRU var val σ@((globalσ,tlσ,i), cache) = case var of
             Nothing  ->  (Map.insert var val someσ, OMap.fromList $ (var, val) : (take (cacheSize - 1) $ OMap.assocs                   cache) )
             Just val ->  (Map.insert var val someσ, OMap.fromList $ (var, val) : (                       OMap.assocs $ OMap.delete var cache) )
 
+
+cacheAwareWriteLRUState :: Monad m => Var -> Val -> StateT FullState m ()
+cacheAwareWriteLRUState var val = do
+    σ <- get
+    put $ cacheAwareWriteLRU var val σ
+    return ()
+
+
 initialCacheState = OMap.fromList [(Global ("___undefined___" ++ (show i)), undefined) | i <- [1..cacheSize]]
 
 exampleSurvey1 :: FullState
@@ -60,34 +76,65 @@ exampleSurvey1 = ((  Map.fromList [(Global "a", 1), (Global "b", 2), (Global "c"
                  )
 
 
+
+cacheAwareLRUEvalB :: Monad m => BoolFunction -> StateT FullState m Bool
+cacheAwareLRUEvalB CTrue     = return True
+cacheAwareLRUEvalB CFalse    = return False
+cacheAwareLRUEvalB (Leq x y) = do
+  xVal <- cacheAwareLRUEvalV x
+  yVal <- cacheAwareLRUEvalV y
+  return $ xVal <= yVal
+cacheAwareLRUEvalB (And b1 b2) = do
+  b1Val <- cacheAwareLRUEvalB b1
+  b2Val <- cacheAwareLRUEvalB b2
+  return $ b1Val && b2Val
+cacheAwareLRUEvalB (Or b1 b2) = do
+  b1Val <- cacheAwareLRUEvalB b1
+  b2Val <- cacheAwareLRUEvalB b2
+  return $ b1Val || b2Val
+cacheAwareLRUEvalB (Not b) = do
+  bVal <- cacheAwareLRUEvalB b
+  return $ not bVal
+
+cacheAwareLRUEvalV :: Monad m => VarFunction -> StateT FullState m Val
+cacheAwareLRUEvalV (Val  x) = return x
+cacheAwareLRUEvalV (Var  x) = cacheAwareReadLRUState x
+cacheAwareLRUEvalV (Plus  x y) = do
+  xVal <- cacheAwareLRUEvalV x
+  yVal <- cacheAwareLRUEvalV y
+  return $ xVal + yVal
+cacheAwareLRUEvalV (Times x y) = do
+  xVal <- cacheAwareLRUEvalV x
+  yVal <- cacheAwareLRUEvalV y
+  return $ xVal * yVal
+cacheAwareLRUEvalV (Neg x) = do
+  xVal <- cacheAwareLRUEvalV x
+  return $ - xVal
+
 {-
-cacheAwareLRUEvalB :: CombinedState -> BoolFunction -> State CacheState Bool
-cacheAwareLRUevalB _ CTrue     = return True
-cacheAwareLRUevalB _ CFalse    = return False
-cacheAwareLRUevalB σ (Leq x y) = do
-  xVal <- 
-  left <- cacheAwareLRUevalB σ evalV σ x <= evalV σ y
-cacheAwareLRUevalB σ (And b1 b2) = evalB σ b1 && evalB σ b2
-cacheAwareLRUevalB σ (Or  b1 b2) = evalB σ b1 || evalB σ b2
-cacheAwareLRUevalB σ (Not b)     = not $ evalB σ b
+instance MonadTrans (StateT s)
+lift :: (Monad m, MonadTrans t) => m a -> t m a
+lift (cacheAwareLRUevalB bf) :: StateT FullState (State FullState) Val
 -}
 
+cacheStepForState :: CFGEdge -> StateT FullState [] FullState
+cacheStepForState (Guard b bf) = do
+        bVal <- cacheAwareLRUEvalB bf
+        σ' <- get
+        if (b == bVal) then return σ' else lift []
+cacheStepForState (Assign x vf) = do
+        xVal <- cacheAwareLRUEvalV vf
+        cacheAwareWriteLRUState x xVal
+        σ' <- get
+        return σ'
+cacheStepForState NoOp = do
+        σ' <- get
+        return σ'
+cacheStepForState (Read  _ _) = undefined
+cacheStepForState (Print _ _) = undefined
+cacheStepForState (Spawn    ) = undefined
+cacheStepForState (Call     ) = undefined
+cacheStepForState (Return   ) = undefined
 
-{-
 cacheStepFor ::  AbstractSemantic FullState
-cacheStepFor e c@((globalσ,tlσ,i), cache)  = step e where
-      σ = globalσ `Map.union` tlσ
-      step :: CFGEdge ->  [FullState]
-      step (Guard b bf)
-        | b == evalB σ bf                = [c]
-        | otherwise                      = []
-      step (Assign x@(Global _)      vf) = [(Map.insert x (evalV σ vf)    globalσ,                              tlσ,                    i)]
-      step (Assign x@(ThreadLocal _) vf) = [(                             globalσ, Map.insert x (evalV σ vf)    tlσ,                    i)]
-      step (Read   x@(Global _)      ch) = [(Map.insert x (head $ i ! ch) globalσ,                              tlσ, Map.adjust tail ch i)]
-      step (Read   x@(ThreadLocal _) ch) = [(                             globalσ, Map.insert x (head $ i ! ch) tlσ, Map.adjust tail ch i)]
-      step (Print  x ch)                 = [c]
-      step (Spawn      )                 = undefined
-      step (NoOp       )                 = [c]
-      step (Call       )                 = [c]
-      step (Return     )                 = [c]
--}
+cacheStepFor e σ = evalStateT (cacheStepForState e) σ

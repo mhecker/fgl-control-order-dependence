9758a38f44afee960a658197581ae6e0bd22f792
Martin Hecker <martin.hecker@kit.edu>
2019-10-23 17:49:03 +0200

    add test cases for crypto examples

--- src/test/Program/Properties/ValidProperties/Cache.hs
+++ src/test/Program/Properties/ValidProperties/Cache.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE CPP #-}
 #include "../Util.cpp"
 
@@ -24,20 +25,21 @@ import Unicode
 import Util(sampleFrom, invert'', moreSeeds)
 
 import Program.Properties.Util
-import Program (Program, tcfg, entryOf, procedureOf, mainThread, observability)
-import Program.For (compileAllToProgram)
+import Program (Program, tcfg, entryOf, exitOf, procedureOf, mainThread, observability)
+import Program.For (compileAllToProgram, For(..))
 import Program.Generator (toProgram, toProgramIntra, toCodeSimple, toCodeSimpleWithArrays, GeneratedProgram, SimpleCFG(..))
+import Program.ExamplesCrypto (br_aes_small_cbcenc_main, br_aes_small_cbcenc_main_ct, br_aes_small_cbcenc_main_ct_precache, mainInput, for2Program)
 
 import IRLSOD(CFGEdge(..), Var(..), Name(..), isGlobalName, globalEmpty, use, def)
 import MicroArchitecturalDependence (stateSets)
-import CacheExecution(twoAddressCode, prependInitialization, prependFakeInitialization, initialCacheState, initialAbstractCacheState, cacheExecution, cacheExecutionLimit, csdMergeDirectOf, cacheCostDecisionGraph, cacheCostDecisionGraphFor, cacheStateGraph, cacheOnlyStepFor, costsFor)
+import CacheExecution(CacheSize, twoAddressCode, prependInitialization, prependFakeInitialization, initialCacheState, initialAbstractCacheState, cacheExecution, cacheExecutionLimit, csdMergeDirectOf, cacheCostDecisionGraph, cacheCostDecisionGraphFor, cacheStateGraph, cacheOnlyStepFor, costsFor)
 import CacheStateDependenceReach(csd''''Of3, csd''''Of4, csdMergeOf)
-import CacheSlice (cacheTimingSliceViaReach)
+import CacheSlice (cacheTimingSliceViaReach, cacheTimingSliceFor)
 
-cache     = defaultMain                               $ testGroup "cache"    [                      mkProp [cacheProps]]
-cacheX    = defaultMainWithIngredients [antXMLRunner] $ testGroup "cache"    [                      mkProp [cacheProps]]
+cache     = defaultMain                               $ testGroup "cache"    [ mkTest [cacheTests], mkProp [cacheProps]]
+cacheX    = defaultMainWithIngredients [antXMLRunner] $ testGroup "cache"    [ mkTest [cacheTests], mkProp [cacheProps]]
 
-cacheSize = 4
+propsCacheSize = 4
 
 cacheProps = testGroup "(concerning cache timing)" [
     testPropertySized 25 "csd only for choice nodes"
@@ -48,15 +50,15 @@ cacheProps = testGroup "(concerning cache timing)" [
                                 b' = fmap twoAddressCode b
                         g = tcfg pr
                         n0 = entryOf pr $ procedureOf pr $ mainThread pr
-                        csdM       = csdMergeDirectOf cacheSize g n0
+                        csdM       = csdMergeDirectOf propsCacheSize g n0
                         
-                        (cs, es)    = stateSets (cacheOnlyStepFor cacheSize) g initialAbstractCacheState n0
+                        (cs, es)    = stateSets (cacheOnlyStepFor propsCacheSize) g initialAbstractCacheState n0
                         
-                        csGraph     = cacheStateGraph cacheSize g initialAbstractCacheState n0
-                        costs       = costsFor cacheSize csGraph
+                        csGraph     = cacheStateGraph propsCacheSize g initialAbstractCacheState n0
+                        costs       = costsFor propsCacheSize csGraph
                         nodesToCsNodes = Map.fromList [ (n, [ y | (y, (n', csy)) <- labNodes csGraph, n == n' ] ) | n <- nodes g]
 
-                        (ccg, cost) = cacheCostDecisionGraphFor cacheSize g csGraph 
+                        (ccg, cost) = cacheCostDecisionGraphFor propsCacheSize g csGraph 
                     in  (∀) (Map.assocs $ invert'' csdM) (\(m, ns) -> 
                           let
                               c = (∃) (lsuc ccg m) (\(n1,l1) ->
@@ -78,8 +80,8 @@ cacheProps = testGroup "(concerning cache timing)" [
                                 b' = fmap twoAddressCode b
                         g = tcfg pr
                         n0 = entryOf pr $ procedureOf pr $ mainThread pr
-                        csd'3 = csd''''Of3 cacheSize g n0
-                        csd'4 = csd''''Of4 cacheSize g n0
+                        csd'3 = csd''''Of3 propsCacheSize g n0
+                        csd'4 = csd''''Of4 propsCacheSize g n0
                     in  csd'3 == csd'4,
     testPropertySized 25 "csdMergeOf == csdMergeDirectOf"
                 $ \generated ->
@@ -89,8 +91,8 @@ cacheProps = testGroup "(concerning cache timing)" [
                                 b' = fmap twoAddressCode b
                         g = tcfg pr
                         n0 = entryOf pr $ procedureOf pr $ mainThread pr
-                        csdM       = csdMergeOf       cacheSize g n0
-                        csdMDirect = csdMergeDirectOf cacheSize g n0
+                        csdM       = csdMergeOf       propsCacheSize g n0
+                        csdMDirect = csdMergeDirectOf propsCacheSize g n0
                     in  csdM == csdMDirect,
     testPropertySized 25 "csdMergeOf ⊑ csd''''Of4"
                 $ \generated ->
@@ -100,8 +102,8 @@ cacheProps = testGroup "(concerning cache timing)" [
                                 b' = fmap twoAddressCode b
                         g = tcfg pr
                         n0 = entryOf pr $ procedureOf pr $ mainThread pr
-                        csdM  = csdMergeOf cacheSize g n0
-                        csd'4 = csd''''Of4 cacheSize g n0
+                        csdM  = csdMergeOf propsCacheSize g n0
+                        csd'4 = csd''''Of4 propsCacheSize g n0
                     in  csdM ⊑ csd'4,
     testPropertySized 25 "csd is sound"
                 $ \generated seed1 seed2 seed3 seed4 ->
@@ -119,7 +121,7 @@ cacheProps = testGroup "(concerning cache timing)" [
                         nodeToVar = Map.fromList $ zip new ((fmap VarName $ Set.toList vars) ++ (fmap ArrayName $ Set.toList varsA))
 
                         g = prependFakeInitialization g0 n0 newN0 varToNode
-                        slicer = cacheTimingSliceViaReach cacheSize g newN0
+                        slicer = cacheTimingSliceViaReach propsCacheSize g newN0
 
 
                         initialFullState   = ((globalEmpty, Map.empty, ()), initialCacheState, 0)
@@ -134,7 +136,7 @@ cacheProps = testGroup "(concerning cache timing)" [
 
                         limit = 9000
                         (execution1, limited1) = assert (length es == 1) $ (head es, (length $ head es) >= limit)
-                          where es = cacheExecutionLimit cacheSize limit g1 initialFullState newN0
+                          where es = cacheExecutionLimit propsCacheSize limit g1 initialFullState newN0
 
                         ms = [ nodes g0 !! (m `mod` (length $ nodes g0)) | m <- moreSeeds seed2 100]
                     in (∀) ms (\m ->
@@ -149,7 +151,7 @@ cacheProps = testGroup "(concerning cache timing)" [
                              g2 = prependActualInitialization initialGlobalState2 initialGlobalState2A
 
                              (execution2, limited2) = assert (length es == 1) $ (head es, (length $ head es) >= limit)
-                               where es = cacheExecutionLimit cacheSize limit g2 initialFullState newN0
+                               where es = cacheExecutionLimit propsCacheSize limit g2 initialFullState newN0
 
                              exec1Obs = filter (\(n,_) -> n ∈ s) $ execution1
                              exec2Obs = filter (\(n,_) -> n ∈ s) $ execution2
@@ -168,3 +170,65 @@ cacheProps = testGroup "(concerning cache timing)" [
                         )
   ]
 
+
+data Aes = Aes {
+    name :: String,
+    cacheSize :: CacheSize,
+    forMain :: For,
+    encryptStateInputNode0 :: Node,
+    keyInputNode :: Node
+  }
+
+aes_main cacheSize = Aes {
+    name = "aes_main",
+    cacheSize = cacheSize,
+    forMain = br_aes_small_cbcenc_main mainInput Skip,
+    encryptStateInputNode0 = 274,
+    keyInputNode = 291
+  }
+
+aes_main_ct cacheSize = Aes {
+    name = "aes_main_ct",
+    cacheSize = cacheSize,
+    forMain = br_aes_small_cbcenc_main_ct mainInput Skip,
+    encryptStateInputNode0 = 17,
+    keyInputNode = 34
+  }
+
+aes_main_ct_precache cacheSize = Aes {
+    name = "aes_main_ct_precache",
+    cacheSize = cacheSize,
+    forMain = br_aes_small_cbcenc_main_ct_precache mainInput Skip,
+    encryptStateInputNode0 = 274,
+    keyInputNode = 291
+  }
+
+
+aesCase expectCT  aes@(Aes { name, cacheSize, forMain, encryptStateInputNode0, keyInputNode }) =
+    testCase (nameA ++ " is " ++ notS ++ " 'constant time' for cacheSize " ++ (show $ cacheSize)) $ (notN $ Set.null $ (Set.fromList ns) ∩ slice) @? ""
+  where notS  = if expectCT then "     " else "*not*"
+        notN  = if expectCT then id      else   not
+        
+        nameA = name ++ (take (22 - (length name)) $ repeat ' ')
+        
+        ns = [encryptStateInputNode0, keyInputNode] ;
+        pr = for2Program $ forMain :: Program Gr ;
+        graph = tcfg pr ;
+        n0 = entryOf pr $ procedureOf pr $ mainThread pr ;
+        nx = exitOf  pr $ procedureOf pr $ mainThread pr ;
+
+        slice = cacheTimingSliceFor cacheSize csdMergeDirectOf graph ns n0 (Set.fromList [nx])
+
+cacheTests = testGroup "(concerning cache timing)" $
+  [ aesCase False $ aes_main              4,
+    -- aesCase False $ aes_main              8,
+    -- aesCase False $ aes_main             12,
+    
+    aesCase True  $ aes_main_ct           4,
+    aesCase True  $ aes_main_ct           8,
+    aesCase True  $ aes_main_ct          12,
+
+    aesCase False $ aes_main_ct_precache  4,
+    aesCase False $ aes_main_ct_precache  8,
+    aesCase True  $ aes_main_ct_precache 12
+  ]

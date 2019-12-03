bafb439cd89c3dc6da82b7e430340043028bb7d7
Martin Hecker <martin.hecker@kit.edu>
2019-12-03 16:46:16 +0100

    more import

--- src/test/Program/Tests.hs
+++ src/test/Program/Tests.hs
@@ -87,6 +87,7 @@ import Data.Graph.Inductive.Query.PostDominance
 import Data.Graph.Inductive.Query.PostDominance.Numbered
 import Data.Graph.Inductive.Query.NTICD
 import Data.Graph.Inductive.Query.OrderDependence
+import Data.Graph.Inductive.Query.Slices.OrderDependence
 import Data.Graph.Inductive.Query.NTIODSlice
 import Data.Graph.Inductive.Query.InfiniteDelay
 import qualified Data.Graph.Inductive.Query.FCACD as FCACD
